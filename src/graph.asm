262198a5fff987bd9878fe51cc1ebfc51bc1a90c
Daniel <dmribeiro24@gmail.com>
2025-11-25 21:05:00 -0300

    Begin graph module

---
+++ src/graph.asm
@@ -0,0 +1,13 @@
+.eqv MAX_ENTITIES 50
+.eqv MAX_EDGES_PER_NODE 50
+
+.data
+
+adj_list: .space 10000		# MAX_ENTITIES * MAX_EDGES_PER_NODE * 4	(50 * 50 * 4)
+adj_weight: .space 10000	# MAX_ENTITIES * MAX_EDGES_PER_NODE * 4	(50 * 50 * 4)
+adj_count: .space 200		# MAX_ENTITIES * 4			(50 * 4)
+
+.text
+
+li $v0 10
+syscall
